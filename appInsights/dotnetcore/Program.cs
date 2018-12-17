9af9a1138d0f9c77af0230967e08b6ec3eb8cd75
Amit Kumar <kumaramit9304@outlook.com>
2018-12-17 15:53:47 +0530

    AppInsight dotnet core sample.

---
+++ appInsights/dotnetcore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dotnetcore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome to App Insights.");
+            Console.ReadLine();
+        }
+    }
+}
