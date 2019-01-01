c85b6a40e8ea6acbe25b2b8968f6dcdb1f372f7d
Amit Kumar <kumaramit9304@outlook.com>
2019-01-01 14:45:27 +0530

    latest changes

---
+++ appInsights/dotnetcore/AppInsight.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace dotnetcore
+{
+    public class AppInsight
+    {
+        private IConfiguration configuration;
+
+        public AppInsight(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task Run()
+        {
+            Console.WriteLine("Printing Appsettings.json");
+            Console.WriteLine("ConnectionStrings:Name :- "+configuration["ConnectionStrings:Name"]);
+            Console.WriteLine("Inside AppInsight Class.");
+            Console.ReadLine();
+        }
+    }
+}
--- appInsights/dotnetcore/Program.cs
+++ appInsights/dotnetcore/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Threading.Tasks;
 
 namespace dotnetcore
@@ -8,6 +11,16 @@ namespace dotnetcore
     /// </summary>
     class Program
     {
+        private static IConfiguration configuration;
+        public static IConfiguration GetConfiguration()
+        {
+            return configuration;
+        }
+
+        public static void SetConfiguration(IConfiguration value)
+        {
+            configuration = value;
+        }
         /// <summary>
         /// Main method
         /// </summary>
@@ -15,8 +28,39 @@ namespace dotnetcore
         /// <returns></returns>
         static async Task Main(string[] args)
         {
+            //Setup DI
+            string environment = "";
+            var builder = new HostBuilder()
+                .UseEnvironment(environment)
+                .ConfigureAppConfiguration
+                ((hostBuilderContext, config) =>
+                {
+                    SetConfiguration(new ConfigurationBuilder()
+                   .SetBasePath(hostBuilderContext.HostingEnvironment.ContentRootPath)
+                   .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
+                   .AddEnvironmentVariables()
+                   .Build());
+                    config.AddConfiguration(GetConfiguration());
+                })
+                .ConfigureServices((builderContext, serviceCollection) =>
+                {
+                    serviceCollection.AddScoped<AppInsight, AppInsight>();
+                })
+                .UseConsoleLifetime();
+
+            //Setup DI
+            //var serviceProvider = (new ServiceCollection())
+            //    .BuildServiceProvider();
             //ApplicationInsight Sample
             Console.WriteLine("Welcome to App Insights.");
+            var host = builder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
+            var appInsight = new AppInsight(configuration);
+            await appInsight.Run();
             Console.ReadLine();
         }
     }
